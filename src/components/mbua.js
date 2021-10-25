8d867c1bb1884401c447cb3a9281e33492bc6714
ellacour <marion.gobin31@gmail.com>
2021-10-25 12:34:21 +0200

    Add style elements from original project

---
+++ src/components/mbua.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+import "./mbua.css"
+
+const Mbua = props => {
+  return (
+    <div id="mbua">
+      <div id="first-column mbua-col">welcome on mbua</div>
+      <div id="second-column mbua-col"></div>
+      <div id="third-column mbua-col"></div>
+      <div id="fourth-column mbua-col"></div>
+    </div>
+  );
+};
+
+export default Mbua;
