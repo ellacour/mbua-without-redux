0fac12ffc2843a5ae7db88ae1b1442b692bd9773
ellacour <marion.gobin31@gmail.com>
2021-10-26 14:13:47 +0200

    Manage user login

---
+++ src/components/login/loginForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./loginForm.css";
+
+
+const LoginForm = props => {
+  const [userName, setUserName] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const userNameChangeHandler = e => {
+    setUserName(e.target.value);
+  };
+
+  const userPasswordChangeHandler = e => {
+    setUserPassword(e.target.value);
+  };
+
+  const submitHandler = e => {
+      e.preventDefault();
+      const loginData = {
+        username: userName,
+        password: userPassword,
+      }
+      props.onUserLogin(loginData);
+      setUserName("");
+      setUserPassword("");
+  }
+
+  return (
+    <div>
+      <form id="Login-form" onSubmit={submitHandler}>
+        <label className="">
+          Username :
+          <input
+            type="text"
+            className="login-form-username"
+            name="username"
+            value={userName}
+            onChange={userNameChangeHandler}
+          />
+        </label>
+        <label className="">
+          Password :
+          <input
+            type="password"
+            className="login-form-password"
+            name="password"
+            value={userPassword}
+            onChange={userPasswordChangeHandler}
+          />
+        </label>
+        <button className="login-form-button" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/login/loginPage.js
@@ -0,0 +1,91 @@
+import React from "react";
+import axios from "axios";
+import Skeleton from "react-loading-skeleton";
+
+import "./loginPage.css";
+import blackPlus from "../../img/mbuA_+ Graphic_black.png";
+import LoginForm from "./loginForm";
+
+const LOGIN_URL = "https://cms.mbu-a.com/wp-json/jwt-auth/v1/token";
+
+const LoginPage = props => {
+  const { loginData, isLogged, userLoginHandler } = props;
+
+  const LoginChangeHandler = userData => {
+    const plusButton = document.getElementById('plus-button')
+    const getToken = async () => {
+      try {
+        const response = await axios.post(LOGIN_URL, userData);
+        const token = await response.data.token;
+        const userName = await response.data.user_nicename;
+        localStorage.setItem('token', token);
+        localStorage.setItem('userName', userName);
+        userLoginHandler(true);
+        plusButton.classList.add('active');
+      } catch (err) {
+        alert(
+          "You are not authorized to access the site. Apply for an account creation"
+        );
+      }
+    };
+    getToken();
+  };
+
+  return (
+    <div id="login">
+      <div className="login-first-column mbua-col"></div>
+      <div className="login-second-column mbua-col">
+        <button id="plus-button" className="login-button">
+          <img className="login-plus" src={blackPlus}></img>
+        </button>
+      </div>
+
+      <div className="login-third-column mbua-col-end">
+        <div className="mbua-adress-card">
+          <p className="title">
+            {loginData.mbua_name + loginData.mbua_title || (
+              <Skeleton count={1} />
+            )}
+          </p>
+          <p className="title">
+            {loginData.business_card_address || <Skeleton count={1} />}
+          </p>
+          <p className="title">
+            {loginData.business_card_city || <Skeleton count={1} />}
+          </p>
+          <p className="title">
+            {loginData.business_card_phone_number || <Skeleton count={1} />}
+          </p>
+          <p className="title">
+            {loginData.business_card_email || <Skeleton count={1} />}
+          </p>
+        </div>
+        {isLogged ? (
+          <div>
+            <div>
+              <p className="light-text italic-text pad-right-125">
+                mbuA is an emergent New York practice dedicated to the
+                exploration and realization of architecture, urbanism, interiors
+                and design. Working across multiple scales, we provide tailored
+                results for clients requiring innovative solutions to complex
+                and often contradictory project requirements. Our commitment is
+                to deliver extraordinary design services and construction
+                excellence with the ambition to materialize architecture as a
+                powerful, evocative, and functional response within an
+                increasingly dissonant built environment.
+              </p>
+            </div>
+            <div>
+              <p className="light-text italic-text ">
+                This website is designed for a large-screen viewing experience.
+              </p>
+            </div>
+          </div>
+        ) : (
+          <LoginForm onUserLogin={LoginChangeHandler} />
+        )}
+      </div>
+    </div>
+  );
+};
+export default LoginPage;
