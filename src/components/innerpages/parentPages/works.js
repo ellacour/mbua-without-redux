db18d5e772f7bd57f7f795dbb22c1fc30659a3aa
ellacour <marion.gobin31@gmail.com>
2021-11-19 16:29:07 +0100

    Display work content

--- src/components/innerpages/parentPages/works.js
+++ src/components/innerpages/parentPages/works.js
@@ -21,46 +21,25 @@ const Works = props => {
     return () => (unmounted = true);
   }, []);
 
-
   const getCurrentWorkContent = content => {
     if (content) {
       const workContent = {
         title: content.title.rendered,
         photos: content.acf.photos,
         text: content.content.rendered
-    }
+      };
       setCurrentWorkContent(workContent);
       setCurrentSlider(content.acf.photos);
-    }  
+    }
   };
-
-  // const worksList = (
-  //   <ul id="works-listing">
-  //     {worksListing.map(work => {
-  //       if (work.acf.project_is_accessible) {
-  //         return (
-  //           <li className="work-menu" key={work.id}>
-  //             <h5>{work.title.rendered}</h5>
-  //           </li>
-  //         );
-  //       } else
-  //         return (
-  //           <li
-  //             className="protected work-menu"
-  //             key={work.id}
-  //             onClick={() => false}
-  //           >
-  //             <h5>{work.title.rendered}</h5>
-  //           </li>
-  //         );
-  //     })}
-  //   </ul>
-  // );
 
   return (
     <div>
       {currentWorkContent ? (
-        <h3>{currentWorkContent.title}</h3>
+        <div>
+          <h3>{currentWorkContent.title}</h3>
+          <div className="" dangerouslySetInnerHTML={{ __html: currentWorkContent.text } || ""} />
+        </div>
       ) : (
         <WorksMenu
           worksListing={worksListing}
