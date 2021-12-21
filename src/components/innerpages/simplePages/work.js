2963c603a53be8f604b507c817c985a3cd069cb8
ellacour <marion.gobin31@gmail.com>
2021-12-21 11:35:15 +0100

    Change button in an aniamted plus button in work

--- src/components/innerpages/simplePages/work.js
+++ src/components/innerpages/simplePages/work.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 
+import blackPlus from "../../../img/mbuA_+ Graphic_black.png";
 const Work = props => {
-    const history = useHistory();
+  const history = useHistory();
   const { currentWorkContent } = props;
 
+  const clickButtonHandler = () => {
+    const workButton = document.getElementById("plus-button-work");
+    workButton.classList.add("active");
+    window.setTimeout(() => history.goBack(), 1000);
+  };
 
   return (
     currentWorkContent && (
       <div className="work-content">
-        <button onClick={() => history.goBack()}>Go Back</button>
+        <button
+          id="plus-button-work"
+          className="return-button"
+          onClick={clickButtonHandler}
+        >
+          <img className="return-plus" src={blackPlus} alt="button"></img>
+        </button>
         <h3 className="work-title-content">{currentWorkContent.title}</h3>
         <div
           className="work-text-content"
